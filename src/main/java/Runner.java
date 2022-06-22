bfaf82e9171409969ed67135daca113431a5817f
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-06-22 14:46:36 +0300

    fix class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,35 +1,21 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Ranner {
+public class Runner {
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива" );
+        System.out.println("Введите размер массива");
         int size = scanner.nextInt();
-        Array array = new Array(size);
-      //  System.out.println(Arrays.toString(array.getRandom(int []array)));
+        System.out.println("Введите дивпазон массива");
+        int numberFirst = scanner.nextInt();
+        int numberSacond = scanner.nextInt();
 
-
-    //    int numberPositve = 0;
-   //     int[] array = new int[100];
-  //      for (int i = 0; i < array.length; i++) {
-
-    //        array[i] = (int) Math.round((Math.random() * (200 + 1) - 100));
-     //   }
-
-
-      //  for (int number : array) {
-     //       if (number % 2 != 0 && number > 0) {
-      //          numberPositve++;
-
-    //        }
-  //      }
-        int result;
-    //    System.out.println(Arrays.toString(array.getRandomArray(result)));
- //       System.out.println("Kоличество положительных нечетных чисел в этом массиве " + numberPositve);
-
-        int RandomArray = 0;
-   //     System.out.println(R);
-
-
+        RandomArray randomArray = new RandomArray(size, numberFirst, numberSacond);
+        System.out.println(Arrays.toString(randomArray.createRandomArray(size, numberFirst, numberSacond)));
+        System.out.println(randomArray.setRandomArray(randomArray.createRandomArray(size, numberFirst, numberSacond)));
     }
 }
